2cef4ea0a88aa01b406f1349888c66eca2b74dce
Nguyen Van Nguyen <nguyennvse180746@fpt.edu.vn>
2026-02-06 12:51:56 +0700

    Enum ngay 6/2

---
+++ SportsBicycleStore/SportsBicycleStore.Domain/Enum/PaymentStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsBicycleStore.Domain.Enum
+{
+    public enum PaymentStatus
+    {
+        //Unpaid = 1,
+        //Paid = 2,
+        Pending = 1,
+        Success = 2,
+        Failed = 3,
+    }
+}
--- SportsBicycleStore/SportsBicycleStore.Domain/Enum/ProductStatus.cs
+++ SportsBicycleStore/SportsBicycleStore.Domain/Enum/ProductStatus.cs
@@ -6,5 +6,11 @@ namespace SportsBicycleStore.Domain.Enum
 {
     public enum ProductStatus
     {
+        Pending_inspection = 1,
+        Available = 2,
+        Inspection_failed = 3,
+        Pending_seller_approval = 4,
+        Sold = 5,
+        Out_of_stock = 6,
     }
 }
